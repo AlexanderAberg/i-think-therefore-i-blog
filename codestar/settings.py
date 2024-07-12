19e4e45d7813e4bf508394ee84ea62e334aec783
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-12 21:39:49 +0000

    Debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-alexanderab-ithinkthere-0g6k19qj5up.ws.codeinstitute-ide.net', '.herokuapp.com']
 
