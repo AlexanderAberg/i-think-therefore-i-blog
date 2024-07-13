0f1537c9f3b74bf5671740dc700ce286b2415d93
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-13 14:05:09 +0000

    Added database migration that didn't work

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-alexanderab-ithinkthere-0g6k19qj5up.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -98,8 +98,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 CSRF_TRUSTED_ORIGINS = [
